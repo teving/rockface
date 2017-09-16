6e27627263d02c68d4a3f9dcbb419c3907b6eace
Kevin Gurton <kevin.gurton@gmail.com>
2017-09-16 21:28:17 +0100

    Fixed up tests

--- client/src/app/problem/location/location-list-component/location-list.component.spec.ts
+++ client/src/app/problem/location/location-list-component/location-list.component.spec.ts
@@ -1,25 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
+import { LocationProvider } from '../location-provider.service';
+import { LocationListItem } from '../location-list-item.model';
 import { LocationListComponent } from './location-list.component';
+import { LocationListItemComponent } from './location-list-item.component';
 
 describe('LocationListComponent', () => {
-   let component: LocationListComponent;
-   let fixture: ComponentFixture<LocationListComponent>;
-
    beforeEach(async(() => {
+      this.locationProvider = {
+         getAll(): Observable<LocationListItem[]> { return Observable.of([]); }
+      };
+
       TestBed.configureTestingModule({
-         declarations: [LocationListComponent]
-      })
-         .compileComponents();
+         providers: [{ provide: LocationProvider, useValue: this.locationProvider }],
+         declarations: [
+            LocationListComponent,
+            LocationListItemComponent
+         ]
+      }).compileComponents();
    }));
 
    beforeEach(() => {
-      fixture = TestBed.createComponent(LocationListComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
+      this.fixture = TestBed.createComponent(LocationListComponent);
+      this.component = this.fixture.componentInstance;
+      this.fixture.detectChanges();
    });
 
-   it('should create', () => {
-      expect(component).toBeTruthy();
+   describe('onInit', () => {
+      it('should create a LocationListItem component for each location', () => {
+         const locations = [{}, {}, {}];
+         const locationProvider = this.fixture.debugElement.injector.get(LocationProvider);
+         spyOn(locationProvider, 'getAll').and.returnValue(Observable.of(locations));
+
+         this.component.ngOnInit();
+         this.fixture.detectChanges();
+
+         const childComponents = this.fixture.debugElement.queryAll(By.directive(LocationListItemComponent));
+         expect(childComponents.length).toBe(3);
+      });
    });
 });
